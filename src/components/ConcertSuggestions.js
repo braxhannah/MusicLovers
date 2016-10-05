1f360a86c7dbd2c024b07f9fa5fea4b557546321
Braxton Hannah <braxhannah@gmail.com>
2016-10-05 09:32:51 -0400

    Ticket Update

--- src/components/ConcertSuggestions.js
+++ src/components/ConcertSuggestions.js
@@ -21,7 +21,9 @@ class ConcertSuggestions extends Component {
           <img src={PierceTheVeil} />
         </CardMedia>
         <CardActions>
-          <FlatButton label='Tickets' />
+          <a href='http://www.ticketmaster.com/journeys-presents-the-made-to-destroy-st-petersburg-florida-10-09-2016/event/0D0050EFB2769A1C?artistid=1487015&majorcatid=10001&minorcatid=60&tm_link=artist_msg-0_0D0050EFB2769A1C&ab=reloaded'>
+            <FlatButton label='Tickets' />
+          </a>
         </CardActions>
       </Card>
       <Card>
@@ -32,7 +34,9 @@ class ConcertSuggestions extends Component {
           <img src={sum41} />
         </CardMedia>
         <CardActions>
-          <FlatButton label='Tickets' />
+          <a href='http://www.ticketmaster.com/sum-41s-dont-call-it-a-st-petersburg-florida-10-08-2016/event/0D005104F5B8BCAD?artistid=706142&majorcatid=10001&minorcatid=60&ab=reloaded'>
+            <FlatButton label='Tickets' />
+          </a>
         </CardActions>
       </Card>
       <Card>
@@ -43,7 +47,9 @@ class ConcertSuggestions extends Component {
           <img src={needToBreathe} />
         </CardMedia>
         <CardActions>
-          <FlatButton label='Tickets' />
+          <a href='http://www.ticketmaster.com/needtobreathe-presents-tour-de-compadres-tampa-florida-10-12-2016/event/0D005076CD9C9178?artistid=1032338&majorcatid=10001&minorcatid=60&tm_link=artist_msg-0_0D005076CD9C9178'>
+            <FlatButton label='Tickets' />
+          </a>
         </CardActions>
       </Card>
       <Card>
@@ -54,7 +60,9 @@ class ConcertSuggestions extends Component {
           <img src={darkMatter} />
         </CardMedia>
         <CardActions>
-          <FlatButton label='Tickets' />
+          <a href='http://www.ticketmaster.com/dark-matter-st-petersburg-florida-12-09-2016/event/0D00513E9FDE9598?artistid=1560128&majorcatid=10001&minorcatid=201'>
+            <FlatButton label='Tickets' />
+          </a>
         </CardActions>
       </Card>
       <Card>
@@ -65,7 +73,9 @@ class ConcertSuggestions extends Component {
           <img src={finishTicket} />
         </CardMedia>
         <CardActions>
-          <FlatButton label='Tickets' />
+          <a href='http://www.ticketmaster.com/finish-ticket-st-petersburg-florida-10-15-2016/event/0D0050CFFAFFC424?artistid=1614473&majorcatid=10001&minorcatid=60'>
+            <FlatButton label='Tickets' />
+          </a>
         </CardActions>
       </Card>
       <Card>
@@ -76,7 +86,9 @@ class ConcertSuggestions extends Component {
           <img src={theSummerSet} />
         </CardMedia>
         <CardActions>
-          <FlatButton label='Tickets' />
+          <a href='http://www.ticketmaster.com/the-summer-set-made-for-you-tampa-florida-10-11-2016/event/0D005112DBABA248?artistid=1485424&majorcatid=10001&minorcatid=60'>
+            <FlatButton label='Tickets' />
+          </a>
         </CardActions>
       </Card>
     </div>
